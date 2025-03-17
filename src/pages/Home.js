f8ec428ae908d644fb2e19ea7454c893f3762ba9
marcelo hernan <marcelo.fernandezc@usm.cl>
2025-03-17 15:51:27 -0300

    prueba de cambbio de texto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Bienvenido a la cabaña</h1>
+      <h1>Hola amor!!! TE AMO</h1>
       <span style={{ fontSize: '100px' }}>❤️</span>
     </div>
   );
